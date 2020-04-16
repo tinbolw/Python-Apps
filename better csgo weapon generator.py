5fc3d2a99b7cd0b1a30982b7d9a9f83212ce65f3
tinbolw <tinbolw@gmail.com>
2020-04-16 12:46:12 -0700

    started better csgo weapon generator.py

---
+++ better csgo weapon generator.py
@@ -0,0 +1,25 @@
+Terrorist = False
+Counter_Terrorist = False
+money = 0
+
+team = input("What team are you on? ")
+if team == "Counter Terrorist" or team == "CT":
+    Counter_Terrorist = True
+elif team == "Terrorists" or team == "T" or team == "Terrorist":
+    Terrorist = True
+else:
+    print("Not a valid team")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
